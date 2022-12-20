1b92d6541a8b0759f3f3b33b72641e3527ae3b22
qsus <106612195+qsus@users.noreply.github.com>
2022-12-20 17:22:36 +0000

    upload code

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # rpi-camera-server
+WARNING: Work in progress!
+
+This is server backend for my project [rpi-camera](https://github.com/qsus/rpi-camera).
+
+## Prerequisites
+* VPS with public IP and apache
+## Usage
+1. Create a file for passwords using this command
+```bash
+htpasswd -c /PATH/TO/passwords USERNAME
+```
+(You can put it wherever you want to, as long as apache has access there. USERNAME, along with password it asks for, will be required to access the webpage.)
+2. Copy .htaccess.example to .htaccess and replace the example values.
---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+/*// Load .env
+$envFile = fopen(".env", "r");
+$envStr = fread($envFile, filesize(".env"));
+fclose($envFile);
+$envArr = array_filter(explode(PHP_EOL, $envStr));
+foreach($envArr as $envEntry) {
+	$envEntryArr = explode("=", $envEntry);
+	$env[$envEntryArr[0]] = $envEntryArr[1];
+}
+unset($envFile, $envStr, $envArr, $envEntry, $envEntryArr);
+// .env is now loaded
+
+if ($_POST["password"] != $env["password"]) {
+	header('Location: /?wrongpassword=true');
+	exit("The entered password is incorrect. Please do not try again, otherwise you could guess it.");
+}
+// Authentication is now done by apache! */
+
+foreach(scandir("videos", SCANDIR_SORT_ASCENDING) as $video) {
+	if ($video == ".." OR $video == ".") continue;
+	
+	/* For every video:*/ ?>
+	
+	<h2>
+		<a href="videos/<?php echo $video; ?>"><?php echo $video ?></a>
+		<sup><a href="videos/<?php echo $video; ?>" download>Stáhnout</a></sup>
+	</h2>
+	<video controls src="videos/<?php echo $video; ?>"></video>
+
+<?php
+	/* End for every video */
+} ?>
